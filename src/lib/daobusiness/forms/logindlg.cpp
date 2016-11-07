8b0d3b2479750087fd7c47e549d8eea68460006a
David Pinelo <david@pinelo.com>
2016-11-07 18:43:09 +0100

    Showing version.

--- src/lib/daobusiness/forms/logindlg.cpp
+++ src/lib/daobusiness/forms/logindlg.cpp
@@ -97,7 +97,7 @@ LoginDlg::LoginDlg(QWidget *parent) :
     }
     ui->lblImage->setPixmap(pixmap);
 
-    QString version = trUtf8("<html><head/><body><p align=\"right\"><span style=\"font-size:6pt;\">Versión: %1</span></p></body></html>").arg(QString(ALEPHERP_REVISION));
+    QString version = trUtf8("<html><head/><body><p align=\"right\"><span style=\"font-size:6pt;\">Versión: <strong>%1</strong></span></p></body></html>").arg(QString(ALEPHERP_REVISION));
     ui->lblVersion->setText(version);
 
     connect (ui->pbOk, SIGNAL(clicked()), this, SLOT(okClicked()));
